57e20ea2471e9d0349ea435261fb4b34f096c8f0
kimw0 <kimw0319@naver.com>
2023-05-06 23:56:38 +0900

    5/6

---
+++ 5/6/upper and lower.py
@@ -0,0 +1,7 @@
+#대소문자가 있는 알파벳에 적용. 숫자 특수문자 한글 등은 불가능
+print("Ups AND Downs".lower()) # ups and downs
+"Ups AND Downs".upper() # UPS AND DOWNS
+"Ups AND Downs".swapcase() # 대문자는 소문자로, 소문자는 대문자로
+                            # uPS and dOWNS
+"Ups AND Downs".capitalize() # 첫번쨰 문자만 대문자로 나머지는 소문자로
+                            # Ups and downs
