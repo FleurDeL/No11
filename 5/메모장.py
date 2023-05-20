aa0ccfdd8ea2eba7c7e592225bcd95f7db7dea74
kimw0 <kimw0319@naver.com>
2023-05-20 23:51:15 +0900

    commit

--- 5/메모장.py
+++ 5/메모장.py
@@ -1,9 +1,6 @@
-n=int(input());l=list(map(int,input().split()))
-print(n,l)
-for i in l:
-    print(i,l)
-    r = 0
-    for j in range(1,i):
-        if i%j==0:r=j
-    if r!=1:n-=1
-print(n)
+s = "a b c d e f g"
+print(f's         : {s}')
+
+r = s.split()
+print(r)
+
