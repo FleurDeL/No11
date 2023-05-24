9fd1f6fbeb8356e58a2d2d899248bdf32c709aee
kimw0 <kimw0319@naver.com>
2023-05-24 22:38:31 +0900

    commit

--- 5/project/クロアチア文字/クロアチア文字.py
+++ 5/project/クロアチア文字/クロアチア文字.py
@@ -6,16 +6,18 @@ j = 0
 for i in range(len(T)):
     j += 1
     t = T[j-1]+T[j]
-    print(t,j)
+    print(t,j, T[j+1])
     if t in a:
         count += 1
         j += 1
+        print('a에 있음')
     elif t not in a:
         count += 1
+        print('a에 없음')
     elif t == 'dz' and T[j+1] == '=':
         count += 1
         j += 2
-        print('yeah')
+        print('dz= 해당')
     print(count)
     if j >= len(T)-1: break
 '''if T[-2]+T[-1] not in a:
--- 5/메모장.py
+++ 5/메모장.py
@@ -1,17 +1,15 @@
-def d(i):
-    if i < 10:
-        return i*2
-    elif 10 <= i and i < 100:
-        return i + i//10 + i%10
-    elif 100 <= i and i < 1000:
-        return i + i//100 + (i%100)//10 + (i%100)%10
-    elif 1000 <= i and i < 10000:
-        return i + i//1000 + (i%1000)//100 + (i%1000)%100//10 + ((i%1000)%100)%10  #1221
-T = [i for i in range(1,10001)]
+def count_substring(main_string, sub_string):
+    count = 0
+    start = 0
+    while True:
+        index = main_string.find(sub_string, start)
+        if index == -1:
+            break
+        count += 1
+        start = index + 1
+    return count
 
-for i in range(1,100):
-    while i < 10000:
-        if d(i) in T:
-            T.remove(d(i))
-        i = d(i)
-for i in range(len(T)): print(T[i])
+a = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+T = 'ddz=z='
+
+print(count_substring(T,a))
